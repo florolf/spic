52990070eb832456d03406b9558e87c3c07781e0
Florian Larysch <fl@n621.de>
2025-12-30 02:22:48 +0100

    add test proof generator

---
+++ test/generator/rekey-policy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+Regenerate a policy with known private keys
+
+Reads a Sigsum policy from stdin and outputs a policy with the same structure
+but with deterministically derived private keys for logs and witensses.
+"""
+
+import sys
+
+from utils import derive
+
+def write_entity(name: str, type_: str) -> None:
+    privkey, pubkey = derive(name)
+    print(f'# private key: {privkey.hex()}')
+    if type_ == 'log':
+        print(f'log {pubkey.hex()} {name}')
+    elif type_ == 'witness':
+        print(f'witness {name} {pubkey.hex()}')
+    else:
+        raise ValueError()
+
+def main():
+    for line in sys.stdin:
+        line = line.strip()
+
+        if line.startswith('log '):
+            _, _, url = line.split()
+            write_entity(url, 'log')
+        elif line.startswith('witness '):
+            _, name, *_ = line.split()
+            write_entity(name, 'witness')
+        else:
+            print(line)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/generator/utils.py
@@ -0,0 +1,35 @@
+import hashlib
+import base64
+import nacl.signing
+
+from typing import Optional
+
+def sha256(data: bytes) -> bytes:
+    return hashlib.sha256(data).digest()
+
+
+def b64enc(data: bytes) -> str:
+    return base64.b64encode(data).decode('ascii')
+
+
+def derive(string: str) -> tuple[bytes, bytes]:
+    priv_bytes = sha256(string.encode())
+    priv = nacl.signing.SigningKey(priv_bytes)
+    return priv_bytes, bytes(priv.verify_key)
+
+
+class PRNG:
+    def __init__(self, seed: bytes):
+        self.seed = seed
+
+    def get_named(self, name: str) -> bytes:
+        return sha256(self.seed + name.encode())
+
+    def get_named_int(self, name: str, limit: Optional[int] = None) -> int:
+        b = self.get_named(name)
+        i = int.from_bytes(b[:8])
+
+        if limit is not None:
+            i = i % limit
+
+        return i
