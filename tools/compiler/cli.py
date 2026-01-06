9f1a6edd3718be64ecb2aa42c005f0dedc3520fb
Florian Larysch <fl@n621.de>
2026-01-06 02:42:11 +0100

    compiler: cli: add more flexible IO options
    Allow reading and writing from stdin/stdout

--- tools/compiler/cli.py
+++ tools/compiler/cli.py
@@ -5,27 +5,50 @@
 # SPDX-License-Identifier: BSD-2-Clause-Patent OR CC0-1.0
 
 import argparse
+import sys
 
 import compiler
 
 from pathlib import Path
+from typing import Optional
+
+def write_blob(target: Optional[str], blob: bytes):
+    if target is None or target == '-':
+        if sys.stdout.isatty():
+            print('refusing to write binary data to terminal', file=sys.stderr)
+            sys.exit(1)
+
+        sys.stdout.buffer.write(blob)
+    else:
+        Path(target).write_bytes(blob)
+
+
+def read_blob(source: Optional[str]) -> bytes:
+    if source is None or source == '-':
+        return sys.stdin.buffer.read()
+    else:
+        return Path(source).read_bytes()
+
 
 def do_policy(args):
-    policy = compiler.SigsumPolicy.from_ascii(args.input.read_text())
+    policy_txt = read_blob(args.input).decode()
+    policy = compiler.SigsumPolicy.from_ascii(policy_txt)
     cpol = policy.compile()
 
-    with args.output.open('wb') as f:
-        f.write(cpol.to_bytes())
+    write_blob(args.output, cpol.to_bytes())
 
 
 def do_dump_policy(args):
-    policy = compiler.SpicPolicy.from_bytes(args.input.read_bytes())
+    policy_raw = read_blob(args.input)
+    policy = compiler.SpicPolicy.from_bytes(policy_raw)
     print(policy.dump())
 
 
 def do_proof(args):
     policy = compiler.SpicPolicy.from_bytes(args.policy.read_bytes())
-    proof = compiler.SigsumProof.from_ascii(args.proof.read_text())
+
+    proof_txt = read_blob(args.input).decode()
+    proof = compiler.SigsumProof.from_ascii(proof_txt)
 
     leaf_keys = []
     for key in args.leaf_key:
@@ -33,12 +56,12 @@ def do_proof(args):
 
     cprf = policy.compile_proof(proof, leaf_keys)
 
-    with args.output.open('wb') as f:
-        f.write(cprf.to_bytes())
+    write_blob(args.output, cprf.to_bytes())
 
 
 def do_dump_proof(args):
-    proof = compiler.SpicProof.from_bytes(args.input.read_bytes())
+    proof_raw = read_blob(args.input)
+    proof = compiler.SpicProof.from_bytes(proof_raw)
     with_sigs = False
     if args.with_sigs:
         with_sigs = True
@@ -52,21 +75,21 @@ def build_parser():
     subparsers = parser.add_subparsers(title="subcommands", dest="command", required=True)
 
     subparser = subparsers.add_parser("policy", help="Compile a policy")
-    subparser.add_argument("input", type=Path)
-    subparser.add_argument("output", type=Path)
+    subparser.add_argument("input", type=str, nargs='?')
+    subparser.add_argument("output", type=str, nargs='?')
 
     subparser = subparsers.add_parser("dump-policy", help="Dump a policy")
-    subparser.add_argument("input", type=Path)
+    subparser.add_argument("input", type=str, nargs='?')
 
     subparser = subparsers.add_parser("proof", help="Compile a proof")
     subparser.add_argument("policy", type=Path)
-    subparser.add_argument("proof", type=Path)
-    subparser.add_argument("output", type=Path)
+    subparser.add_argument("proof", type=str)
+    subparser.add_argument("output", type=str)
     subparser.add_argument("leaf_key", type=str, nargs='+')
 
     subparser = subparsers.add_parser("dump-proof", help="Dump a proof")
-    subparser.add_argument("input", type=Path)
     subparser.add_argument("--with-sigs", action='store_true')
+    subparser.add_argument("input", type=str, nargs='?')
 
     return parser
 
